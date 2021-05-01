6a83fe6c69ef04c6f971d35db8f954a6715c2131
John Nover <jnover@ucsc.edu>
2021-05-01 10:42:38 -0500

    feat(result): display one result

--- car-site/src/index.js
+++ car-site/src/index.js
@@ -3,37 +3,68 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Result extends React.Component {
-  render() {
-    return (
-      <div>results shown here</div>
-    );
-  }
+	render() {
+		let car_name = "2007 Jeep Commander",
+			vin = "3C3CFFJH2ET392187",
+			dealerCost = 7584.97,
+			dateListed = "2020-10-12 23:25:58"
+		
+		return (
+			<div className="car-information">
+				<div name="car-vin">
+					{vin}
+				</div>
+				<div name="car-name">
+					{car_name}
+				</div>
+				<div name="car-dealercost">
+					{dealerCost}
+				</div>
+				<div name="car-datelisted">
+					{dateListed}
+				</div>
+			</div>
+		);
+	}
 }
 
 class SearchBox extends React.Component {
-  render() {
-    return (
-      <div>
-		<input type="text" width="10px" placeholder="Search for a car" value="" />
-		<Result />
-	  </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {search: ""};
+	}
+	
+	searchCars = (e) => {
+		this.setState({search: e.target.value});
+	}
+	
+	render() {
+		return (
+			<div>
+				<input 
+					onChange={this.searchCars}
+					placeholder="Search for a car" 
+					type="text" 
+					/>
+				<Result />
+			</div>
+		);
+	}
 }
 
 class Selector extends React.Component {
-  render() {
-    return (
-      <div className="selector">
-        <SearchBox />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="selector">
+				<SearchBox />
+			</div>
+		);
+	}
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Selector />,
-  document.getElementById('root')
+	<Selector />,
+	document.getElementById('root')
 );

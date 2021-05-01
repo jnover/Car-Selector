0cab3163c2a04e5101ed8b866a0272e481f71212
John Nover <jnover@ucsc.edu>
2021-05-01 09:57:47 -0500

    feat(web): add base react components for searching

--- car-site/src/index.js
+++ car-site/src/index.js
@@ -2,58 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Result extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
-      </button>
+      <div>results shown here</div>
     );
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
+class SearchBox extends React.Component {
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+		<input type="text" width="10px" placeholder="Search for a car" value="" />
+		<Result />
+	  </div>
     );
   }
 }
 
-class Game extends React.Component {
+class Selector extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div className="selector">
+        <SearchBox />
       </div>
     );
   }
@@ -62,6 +34,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Selector />,
   document.getElementById('root')
 );

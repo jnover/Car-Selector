e2090442e3ab660352cc67f54d6ee851a30e2f31
John Nover <jnover@ucsc.edu>
2021-05-01 11:07:10 -0500

    feat(result): display multiple results

--- car-site/src/index.js
+++ car-site/src/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+
 class Result extends React.Component {
 	render() {
-		let car_name = "2007 Jeep Commander",
-			vin = "3C3CFFJH2ET392187",
-			dealerCost = 7584.97,
-			dateListed = "2020-10-12 23:25:58"
+		let car_name = this.props.name, 
+			vin = this.props.vin,
+			dealerCost = this.props.dealerCost,
+			dateListed = this.props.dateListed;
 		
 		return (
 			<div className="car-information">
 				<div name="car-vin">
-					{vin}
+					VIN: {vin}
 				</div>
 				<div name="car-name">
-					{car_name}
+					NAME: {car_name}
 				</div>
 				<div name="car-dealercost">
-					{dealerCost}
+					DEALERCOST: {dealerCost}
 				</div>
 				<div name="car-datelisted">
-					{dateListed}
+					DATELISTED: {dateListed}
 				</div>
 			</div>
 		);
@@ -39,14 +41,24 @@ class SearchBox extends React.Component {
 	}
 	
 	render() {
+		var results = [{vin:"123", name:"car1", dealerCost:1.23, dateListed:"05/01/2021"},
+			{vin:"234", name:"car2", dealerCost:2.34, dateListed:"05/01/2021"},
+			{vin:"345", name:"car3", dealerCost:3.45, dateListed:"05/01/2021"},
+		];
 		return (
 			<div>
 				<input 
 					onChange={this.searchCars}
 					placeholder="Search for a car" 
 					type="text" 
-					/>
-				<Result />
+				/>
+				{results.map(i => {
+					return <Result 
+						vin={i.vin} 
+						name={i.name}
+						dealerCost={i.dealerCost} 
+						dateListed={i.dateListed} />
+				})}
 			</div>
 		);
 	}
